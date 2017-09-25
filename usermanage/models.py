35baaf7fba2a0b439826bcd93a2c8608d50e4017
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-25 19:54:52 +0700

    add customer and store model to db

--- usermanage/models.py
+++ usermanage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30, null=True)
+    last_name = models.CharField(max_length=30, null=True)
+
+class Store(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    store_name = models.CharField(max_length=30)
