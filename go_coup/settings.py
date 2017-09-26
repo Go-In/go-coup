c268279cf83c657842f1b0a9dd1e2b5cb218865b
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-27 01:29:31 +0700

    add django-querycount

--- go_coup/settings.py
+++ go_coup/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'querycount.middleware.QueryCountMiddleware',
 ]
 
 ROOT_URLCONF = 'go_coup.urls'
