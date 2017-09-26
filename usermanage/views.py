d9b0beb12539d80ab9c8c11651602283e4d27eb6
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-26 13:34:59 +0700

    redirected from signin page when user is signed in

--- usermanage/views.py
+++ usermanage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -28,6 +28,8 @@ def storeSignup(request):
     return redirect('index:index')
 
 def signin(request):
+    if request.user.is_authenticated:
+        return redirect('index:index')
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
