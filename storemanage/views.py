b126156683ad6cb3616cb45aeca98f66f8f35822
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-27 18:19:34 +0700

    create currency creation

--- storemanage/views.py
+++ storemanage/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import Currency
 # Create your views here.
+def currencyRegister(request):
+    user = request.user
+    if request.method == 'GET':
+        return render(request,'storemanage/currency-form.html')
+    data = request.POST
+    currency = Currency(store=user,name=data['name'])
+    currency.save()
+    return redirect('index:index')
