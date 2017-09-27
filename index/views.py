60a8f05934b391be0c0b5108e72a6576c3b6cbd8
Weawprach <tidjungs@Weawprachs-MacBook-Pro.local>
2017-09-27 22:16:47 +0700

    add link from index to detail

--- index/views.py
+++ index/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index/index.html', {})
+
+def detail(request):
+    return render(request, 'index/detail.html', {})
