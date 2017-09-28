fe3a50a840dfda2888015280b1f4b20e791c9543
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-28 10:03:45 +0700

    fix POST keyerror bug

--- storemanage/views.py
+++ storemanage/views.py
@@ -26,8 +26,8 @@ def ticketRegister(request):
     ticket_attrib = {k:v for k,v in data.items() if v != ''}
     print(ticket_attrib)
     ticket_attrib.pop('csrfmiddlewaretoken')
-    ticket_attrib['is_period'] = True if ticket_attrib['is_period']=='period' else False
-    ticket_attrib['is_limit'] = True if ticket_attrib['is_limit']=='limit' else False
+    ticket_attrib['is_period'] = True if 'is_period' in ticket_attrib else False
+    ticket_attrib['is_limit'] = True if 'is_limit' in ticket_attrib else False
     ticket_attrib['currency'] = Currency.objects.get(pk=ticket_attrib['currency'])
     ticket_attrib['store'] = user
     ticket = Ticket(**ticket_attrib)
