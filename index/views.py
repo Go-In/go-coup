9e6f1ea39fea8facf0932387d5a82cba1dc0d46b
Weawprach <tidjungs@Weawprachs-MacBook-Pro.local>
2017-09-28 17:03:41 +0700

    render detail template from Ticket model

--- index/views.py
+++ index/views.py
@@ -4,16 +4,15 @@ from storemanage.models import Ticket
 # Create your views here.
 def index(request):
     tickets = Ticket.objects.all()
-    a = [];
-    for i in range(10):
-        a.append(tickets[0])
-    print(a)
     return render(request, 'index/index.html', {
-        'tickets': a
+        'tickets': tickets
     })
 
-def detail(request, coupon_id):
-    return render(request, 'index/detail.html', {})
+def detail(request, ticket_id):
+    ticket = Ticket.objects.get(pk=ticket_id)
+    return render(request, 'index/detail.html', {
+        'ticket' : ticket
+    })
 
 def profile(request):
     return render(request, 'index/profile.html', {})
