d18d770d32d3b0a875d87e83dd4a83ba7edf11bd
psychoAB <dome.2499@hotmail.com>
2017-09-28 23:48:41 +0700

    add register store view test

--- usermanage/tests.py
+++ usermanage/tests.py
@@ -4,3 +4,7 @@ class usermanageViewsTestCase(TestCase):
     def test_user_signup(self):
         resp = self.client.get('/user/signup', follow = True)
         self.assertEqual(resp.status_code, 200)
+
+    def test_store_signup(self):
+        resp = self.client.get('/user/store-signup', follow = True)
+        self.assertEqual(resp.status_code, 200)
