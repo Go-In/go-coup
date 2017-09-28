8920f4b2dff2a3fb087e139fe0b8cc9bcdaa5a4c
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-28 21:34:50 +0700

    create coupon model

--- customermanage/models.py
+++ customermanage/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from storemanage.models import Currency
+from storemanage.models import Currency, Ticket
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -7,3 +7,9 @@ class Wallet(models.Model):
     currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.PositiveIntegerField(default=2)
+
+class Coupon(models.Model):
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    remaining_date = models.DateTimeField(null=True)
+    active = models.BooleanField(default=True)
