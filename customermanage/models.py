15a68a8a67b85b22b1134f5f023040ebb6b3c556
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-28 17:46:01 +0700

    create wallet model

--- customermanage/models.py
+++ customermanage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from storemanage.models import Currency
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Wallet(models.Model):
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField(default=2)
