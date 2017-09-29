2400d1c872e195c02d0bcde005f3e98497dabce1
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-29 23:20:02 +0700

    now can minus money from wallet

--- market/views.py
+++ market/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from storemanage.models import Currency, Ticket
 from usermanage.models import Store
 from customermanage.models import Wallet
@@ -10,5 +10,12 @@ def purchase(request):
         return HttpResponseBadRequest()
     user = request.user
     ticket_id = request.POST['ticket_id']
+    print(ticket_id)
     ticket = get_object_or_404(Ticket, pk=ticket_id)
-    return render('index:index')
+    wallet,create = Wallet.objects.get_or_create(user=user, currency = ticket.currency)
+    print(wallet.amount, create)
+    if ticket.price <= wallet.amount:
+        wallet.amount = wallet.amount - ticket.price
+        wallet.save()
+        print(wallet.amount)
+    return redirect('index:detail',ticket_id = ticket_id)
