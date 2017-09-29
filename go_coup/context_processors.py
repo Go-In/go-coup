9de2d0fb118d32cf398d1458aa2bd652e8dc5828
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-29 22:20:21 +0700

    edit context_processor

--- go_coup/context_processors.py
+++ go_coup/context_processors.py
@@ -1,3 +1,3 @@
 def groups(request):
-    groups = list(request.user.groups.values_list('name', flat=True)
-    return {'isCustomer':'customer' in groups, 'isStore':'store' in groups}
+    groups = list(request.user.groups.values_list('name', flat=True))
+    return {'is_customer':'customer' in groups, 'is_store':'store' in groups}
