d7b9f5f0a717bffcfbc89ea8270f89e2e28db2ed
psychoAB <dome.2499@hotmail.com>
2017-09-29 14:48:12 +0700

    add profile name of user and store testing

--- usermanage/tests.py
+++ usermanage/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+from . import models
 
 class usermanageViewsTestCase(TestCase):
     def test_user_signup(self):
@@ -8,3 +11,13 @@ class usermanageViewsTestCase(TestCase):
     def test_store_signup(self):
         resp = self.client.get('/user/store-signup', follow = True)
         self.assertEqual(resp.status_code, 200)
+
+    def test_user_str(self):
+        customer_user = User.objects.create_user('customer_username')
+        customer = models.Customer(user = customer_user, first_name = 'customer_firstname')
+        self.assertEqual(str(customer), customer.first_name)
+
+    def test_store_title(self):
+        store_user = User.objects.create_user('store_username')
+        store = models.Store(user = store_user, store_name = 'store_storename' )
+        self.assertEqual(str(store), store.store_name)
