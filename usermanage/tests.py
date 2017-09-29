e571ca56bf98a636f8180100278027f8f895ed8e
psychoAB <dome.2499@hotmail.com>
2017-09-30 02:30:29 +0700

    add loged_user_not_be_able_to_login_again test

--- usermanage/tests.py
+++ usermanage/tests.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from . import models
 
 class usermanageViewsTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user('testing_user', password = 'testing_password')
+
     def test_customer_register(self):
         resp = self.client.get('/user/register', follow = True)
         self.assertEqual(resp.status_code, 200)
@@ -15,6 +18,11 @@ class usermanageViewsTestCase(TestCase):
     def test_user_login(self):
         resp = self.client.get('/user/login', follow = True)
         self.assertEqual(resp.status_code, 200)
+
+    def test_loged_user_not_be_able_to_login_again(self):
+        resp = self.client.login(username = 'testing_user', password = 'testing_password')
+        resp = self.client.get('/user/login', follow = True)
+        self.assertEqual(resp.templates[0].name, 'index/index.html')
 
     def test_customer_str(self):
         customer_user = User.objects.create_user('customer_username')
