5a40fd40175d2471c419d8dce15fbc9b0ca4728e
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-30 07:26:49 +0700

    add JSONfield to usermanage

--- usermanage/models.py
+++ usermanage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
 
@@ -9,6 +10,7 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=30, null=True)
     birthdate = models.DateField(null=True)
     tel = models.CharField(max_length=12, null=True)
+    attribute = JSONField(default = dict)
     def __str__(self):
         return self.user
 
@@ -16,6 +18,8 @@ class Store(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     store_name = models.CharField(max_length=30)
     tel = models.CharField(max_length=12, null=True)
+    attribute = JSONField(default = dict)
+
     def __str__(self):
         return self.user
 
