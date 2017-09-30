c1c55759a099296c2187d663161c991ab87a0b29
M.Charattrakool <m.charattrakool@gmail.com>
2017-09-30 15:03:41 +0700

    add field

--- usermanage/models.py
+++ usermanage/models.py
@@ -11,6 +11,7 @@ class Customer(models.Model):
     birthdate = models.DateField(null=True)
     tel = models.CharField(max_length=12, null=True)
     attribute = JSONField(default = dict())
+    thumbnail_image_url = models.CharField(max_length=1023, null=True)
     available = models.BooleanField(default=True)
 
     def __str__(self):
@@ -21,6 +22,7 @@ class Store(models.Model):
     store_name = models.CharField(max_length=30)
     tel = models.CharField(max_length=12, null=True)
     attribute = JSONField(default = dict())
+    thumbnail_image_url = models.CharField(max_length=1023, null=True)
     available = models.BooleanField(default=True)
 
     def __str__(self):
