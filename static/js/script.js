41bd3065152491077eece5f6a278431b5e19dea0
tidjungs <waewprach.s@ku.th>
2017-10-01 13:03:26 +0700

    fix handler decrement ticket

--- static/js/script.js
+++ static/js/script.js
@@ -10,7 +10,6 @@ const addItemToCart = (cart, itemId) => {
 const handlerAddToCart = (itemId, name) => {
   const cart = getCartItem();
   const AddedCart = addItemToCart(cart, itemId);
-  console.log(AddedCart);
   localStorage.setItem('gocoup-cart', JSON.stringify(cart));
   swal(
     'Add Success !',
@@ -40,4 +39,53 @@ const cartItemToObject = (storage = localStorage) => {
     }
   })
   return obj;
+}
+
+function incrementTicket(ticket_id) {
+  var cartItems = getCartItem();
+  cartItems = addItemToCart(cartItems, ticket_id)
+  localStorage.setItem('gocoup-cart', JSON.stringify(cartItems));  
+  renderTicket(ticket_id, cartItems[ticket_id]);
+}
+
+function renderTicket(ticket_id, quantity) {
+  var numElement = document.getElementById('ticket_' + ticket_id);
+  numElement.innerText = 'quantity: ' + quantity;
+  $('#buy-btn').val(JSON.stringify(cartItemToObject()))
+}
+
+function decrementTicket(cart, ticket_id) {
+  cart[ticket_id]--;
+  return cart;
+}
+
+function ticketLeft(cart, ticket_id) {
+  return cart[ticket_id] > 0;
+}
+
+function handlerDelete(ticket_id) {
+  var cart = decrementTicket(getCartItem(), ticket_id);
+  if (ticketLeft(cart, ticket_id)) {  
+    localStorage.setItem('gocoup-cart', JSON.stringify(cart));
+    renderTicket(ticket_id, cart[ticket_id]);
+  } else {
+    deleteTicketFromCart(ticket_id)
+  }
+}
+
+function deleteTicketFromCart(ticket_id) {
+  swal({
+    title: 'เอา ticket นี้ออกจาก cart !',
+    type: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'ใช่',
+    cancelButtonText: 'ไม่',
+  }).then(function() {
+    var cartItems = getCartItem();
+    cartItems = cartItems.filter(c => Object.keys(c)[0] !== String(ticket_id));
+    var cartString = cartItems.map(c => JSON.stringify(c));
+    localStorage.setItem('gocoup-cart', [...new Set(cartString)]);
+    document.getElementById('row_' + ticket_id).remove();
+    history.pushState(null, '', getCartUrl());
+  })
 }
