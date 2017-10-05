bf0dfe5b127162301e6890fc08309dd38aa4d6b8
M.Charattrakool <m.charattrakool@gmail.com>
2017-10-05 09:39:05 +0700

    fix storemanage bug

--- storemanage/models/Ticket.py
+++ storemanage/models/Ticket.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import JSONField
+from .Currency import Currency
 
 # Create your models here.
 
