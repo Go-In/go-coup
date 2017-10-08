d86b2578285d76e53c5d7d32bdb1feaf9a2ce364
MambaClaw <nickidaiki@gmail.com>
2017-10-08 15:46:28 +0700

    tel only check too long

--- usermanage/views/customerRegister.py
+++ usermanage/views/customerRegister.py
@@ -6,8 +6,10 @@ from django.contrib.auth.decorators import login_required, user_passes_test, per
 from django.contrib.auth.forms import UserCreationForm
 from customermanage.models import Coupon, Wallet
 from storemanage.models import Ticket
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 # Create your views here.
-from usermanage import models
+from usermanage import models   
 
 def customerRegister(request):
     if request.user.is_authenticated:
@@ -19,7 +21,49 @@ def customerRegister(request):
     # check user already exits
     if User.objects.filter(username=data['username']).exists():
         return render(request, 'usermanage/register-customer.html', {
-                'error' : True,
+                'username_error' : True,
+                })
+
+    # check first name is too long
+    if len(data['first_name']) > 30:
+        return render(request, 'usermanage/register-customer.html', {
+                'firstname_len_error' : True,
+                })
+
+    # check first name is too short
+    if len(data['last_name']) > 30:
+        return render(request, 'usermanage/register-customer.html', {
+                'lastname_len_error' : True,
+                })
+
+    #check tel
+    if len(data['tel']) > 12:
+        return render(request, 'usermanage/register-customer.html', {
+                'tel_len_error' : True,
+                })
+
+    # check password is too short
+    if len(data['password']) < 8:
+        return render(request, 'usermanage/register-customer.html', {
+                'short_password_error' : True,
+                })
+
+    # check password is too long
+    if len(data['password']) > 20:
+        return render(request, 'usermanage/register-customer.html', {
+                'long_password_error' : True,
+                })   
+
+    # check email
+    try:
+        validate_email(data['email'])
+    except ValidationError as e:
+        return render(request, 'usermanage/register-customer.html', {
+                'email_error' : True,
+                })
+    else:
+        return render(request, 'usermanage/register-customer.html', {
+                'email_error' : False,
                 })
 
     user = User.objects.create_user(username = data['username'], password = data['password'], email = data['email'])
