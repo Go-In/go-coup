876050921b3bb2db0dbe7ee425e05cc5a7e33355
RocKIn0X <dantai.l@ku.th>
2017-10-08 15:15:06 +0700

    fix value not show when search

--- storemanage/search_indexes.py
+++ storemanage/search_indexes.py
@@ -10,6 +10,10 @@ class TicketIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.EdgeNgramField(indexed=True, document=True, use_template=True)
     name = indexes.CharField(stored=True, indexed=False, model_attr='name')
     detail = indexes.CharField(stored=True, indexed=False, model_attr='detail')
+    price = indexes.CharField(stored=True, indexed=False, model_attr='price')
+    expire_date = indexes.DateTimeField(stored=True, indexed=False, model_attr='expire_date')
+    currency = indexes.CharField(stored=True, indexed=False, model_attr='currency')
+    ticket_image_url = indexes.CharField(stored=True, indexed=False, model_attr='ticket_image_url')
     # name = indexes.EdgeNgramField(model_attr='name')
     # detail = indexes.EdgeNgramField(model_attr='detail')
  
