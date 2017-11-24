cb74b698a7dff694074744f3352f9e842448a105
RocKIn0X <dantai.l@ku.th>
2017-11-24 19:16:52 +0700

    check condition of get-coupon

--- web/storemanage/views/getCoupon.py
+++ web/storemanage/views/getCoupon.py
@@ -11,12 +11,20 @@ def getCoupon(request, key):
     url_get_pk = 'http://coupon-qr-gen:8082/load'
     payload = {'key':  key}
     req = requests.post(url_get_pk, data=payload)
-    pk = req.json()['Pk']
+    found = False
+    if req.json()['Status'] == 'Ok':
+        found = True
+        pk = req.json()['Pk']   
 
-    coupon = Coupon.objects.get(id=pk)
-    coupon.active = False
-    coupon.save()
-
-    return render(request, 'store/get-coupon.html', {
-            'coupon': coupon
+        coupon = Coupon.objects.get(id=pk)
+        coupon.active = False
+        coupon.save()
+        return render(request, 'store/get-coupon.html', {
+            'coupon': coupon,
+            'found': found,
         })
+    else:
+        return render(request, 'store/get-coupon.html', {
+            'found': found,
+        })
+    
