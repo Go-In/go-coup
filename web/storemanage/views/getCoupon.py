3defffd71f8a4a6c517b4af8c428aecba1970a68
RocKIn0X <dantai.l@ku.th>
2017-11-24 14:22:34 +0700

    alert coupon from key

--- web/storemanage/views/getCoupon.py
+++ web/storemanage/views/getCoupon.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.staticfiles.views import serve
 from storemanage.models import Ticket, Currency
-from customermanage.models import Wallet, Qrcode
+from customermanage.models import Coupon
 
 from django.http import JsonResponse
 
 import requests
 
-def getCoupon(request):
+def getCoupon(request, key):
+    url_get_pk = 'http://coupon-qr-gen:8082/load'
+    payload = {'key':  key}
+    req = requests.post(url_get_pk, data=payload)
+    pk = req.json()['Pk']
+
+    coupon = Coupon.objects.get(id=pk)
+    print(coupon.id)
+    # print(coupon.ticket.ticket_image_url)
+
     return render(request, 'store/get-coupon.html', {
-            
+            'coupon': coupon
         })
