8dccfa6bd629e80451cfec0bf7c3c15b88b39b5b
tidjungs <waewprach.s@ku.th>
2017-11-24 12:38:31 +0700

    connect with mlab and add config

--- notification_server/server.js
+++ notification_server/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+
+mongoose.connect(config.get('DB_URL'))
 const app = express()
 
 app.get('/status', (req, res) => {
   res.send('Server is running')
 })
 
-const PORT = process.env.PORT || 8216;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, function () {
   console.log(`push_server listening on port ${PORT}!`)
 })
+
 
