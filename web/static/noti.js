632fcc505bb93b172b16e57bd31d6e0b00bc170a
tidjungs <waewprach.s@ku.th>
2017-11-24 14:03:13 +0700

    add client noti in service worker and subscribe

--- web/static/noti.js
+++ web/static/noti.js
@@ -18,6 +18,32 @@ function askPermission() {
 function subscribeStore(storeId, userId) {
   askPermission()
   .then(function() {
-    console.log('sss', storeId, userId)
+    return navigator.serviceWorker.ready
+  })
+  .then(function(reg) {
+    const subscribeOptions = {
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array('BAjY5Vq4p2wwLa-tdde3ip7uBxP02MN_4GTvQv4FDQTCs3b_tWl-eZoqtLhhtp_gPmvnk-CtS4zL4kRyeSb6f6I')
+    }
+    return reg.pushManager.subscribe(subscribeOptions)
+  })
+  .then(function() {
+    console.log('sucess')
   })
 }
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
--- web/static/sw.js
+++ web/static/sw.js
@@ -13,3 +13,14 @@ toolbox.router.get('/static*', toolbox.networkFirst);
 
 toolbox.router.get('/', toolbox.networkFirst);
 toolbox.router.get('/user/coupon', toolbox.networkFirst);
+
+
+self.addEventListener('push', function(event) {
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
